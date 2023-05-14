04b4807bdef34e3797201d3303ae37347e0b0b8b
Arkeonn <tgeoffron@myges.fr>
2023-05-14 18:25:40 +0200

    Feat: First draft for Authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { AUTH_SERVICE } from 'src/constants';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +13,15 @@ export class AuthController {
   @Get()
   public index() {
     return this.authProxy.send('index', {});
+  }
+
+  @Post('/register')
+  public register(@Body() data: RegisterDto) {
+    return this.authProxy.send('register', data);
+  }
+
+  @Post('/login')
+  public login(@Body() data: LoginDto) {
+    return this.authProxy.send('login', data);
   }
 }
