e34c6853fd758a625213517d0838d10bf68ee268
Coraline <cesedji@myges.fr>
2023-05-22 21:03:17 +0200

    Feat : add crud for api exams

--- src/exams/exams.controller.ts
+++ src/exams/exams.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { EXAMS_SERVICE } from 'src/constants';
 import { catchRpcException } from 'src/exceptions/exceptions.pipe';
+import { AbstractBody } from 'src/types';
 
 @Controller('exams')
 export class ExamsController {
@@ -17,5 +28,33 @@ export class ExamsController {
   @Get('/kill')
   public kill() {
     return this.examsProxy.send('kill', {}).pipe(catchRpcException);
+  }
+
+  @Get()
+  public findAll() {
+    return this.examsProxy.send('findAll', {});
+  }
+
+  @Get(':id')
+  public findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.examsProxy.send('findOne', id);
+  }
+
+  @Post()
+  public create(@Body() body: AbstractBody) {
+    return this.examsProxy.send('create', body);
+  }
+
+  @Delete(':id')
+  public remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.examsProxy.send('remove', id);
+  }
+
+  @Patch(':id')
+  public update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() body: AbstractBody,
+  ) {
+    return this.examsProxy.send('update', { id, body });
   }
 }
