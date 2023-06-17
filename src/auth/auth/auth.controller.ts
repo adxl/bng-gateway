17b518eaa24124c901f4f04d37e273694b1fceef
Adel Sen <adelsenhadjii@gmail.com>
2023-06-17 21:41:39 +0200

    Feat: add /me route

--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { AUTH_SERVICE } from 'src/constants';
 import { catchRpcException } from 'src/exceptions/exceptions.pipe';
@@ -7,6 +7,11 @@ import { AbstractBody } from 'src/types';
 @Controller('auth')
 export class AuthController {
   public constructor(@Inject(AUTH_SERVICE) private readonly authProxy: ClientProxy) {}
+
+  @Get('/me')
+  public me(@Headers('authorization') token: string) {
+    return this.authProxy.send('auth.me', { token }).pipe(catchRpcException);
+  }
 
   @Post('/register')
   public register(@Body() body: AbstractBody) {
