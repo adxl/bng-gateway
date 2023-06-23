4b8fcf44b9ddc4f87b7f6f641a9d4715e8370007
Océane <oceane.renoux@hotmail.fr>
2023-06-23 15:12:59 +0200

    feat : crud event

--- src/events/events/events.controller.ts
+++ src/events/events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { EVENTS_SERVICE } from 'src/constants';
 import { AbstractBody } from 'src/types';
@@ -11,5 +11,25 @@ export class EventsController {
   @Post()
   public create(@Body() body: AbstractBody) {
     return this.eventsProxy.send('events.create', body).pipe(catchRpcException);
+  }
+
+  @Get()
+  public findAll() {
+    return this.eventsProxy.send('events.findAll', {}).pipe(catchRpcException);
+  }
+
+  @Get(':id')
+  public findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.eventsProxy.send('events.findOne', id).pipe(catchRpcException);
+  }
+
+  @Patch(':id')
+  public update(@Param('id', ParseUUIDPipe) id: string, @Body() body: AbstractBody) {
+    return this.eventsProxy.send('events.update', { id, body }).pipe(catchRpcException);
+  }
+
+  @Delete(':id')
+  public remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.eventsProxy.send('events.remove', id).pipe(catchRpcException);
   }
 }
