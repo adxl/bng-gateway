141168f412b5f02d74eeda9b6dcc03cf2fba2cb8
Océane <oceane.renoux@hotmail.fr>
2023-06-24 16:43:25 +0200

    feat : clean route not use

--- src/events/events-winners/events-winners.controller.ts
+++ src/events/events-winners/events-winners.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Inject, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, ParseUUIDPipe, Patch } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { EVENTS_SERVICE } from 'src/constants';
 import { AbstractBody } from 'src/types';
@@ -8,29 +8,9 @@ import { catchRpcException } from 'src/exceptions/exceptions.pipe';
 export class EventsWinnersController {
   public constructor(@Inject(EVENTS_SERVICE) private readonly eventsProxy: ClientProxy) {}
 
-  @Post()
-  public create(@Body() body: AbstractBody) {
-    return this.eventsProxy.send('eventsWinners.create', body).pipe(catchRpcException);
-  }
-
-  @Get()
-  public findAll() {
-    return this.eventsProxy.send('eventsWinners.findAll', {}).pipe(catchRpcException);
-  }
-
-  @Get(':event')
-  public findOne(@Param('event', ParseUUIDPipe) event: string, @Body() body: AbstractBody) {
-    return this.eventsProxy.send('eventsWinners.findOne', { event, body }).pipe(catchRpcException);
-  }
-
   @Patch(':event')
   public update(@Param('event', ParseUUIDPipe) event: string, @Body() body: AbstractBody) {
     return this.eventsProxy.send('eventsWinners.update', { event, body }).pipe(catchRpcException);
-  }
-
-  @Delete(':event')
-  public remove(@Param('event', ParseUUIDPipe) event: string, @Body() body: AbstractBody) {
-    return this.eventsProxy.send('eventsWinners.remove', { event, body }).pipe(catchRpcException);
   }
 
   @Get('user/:id')
