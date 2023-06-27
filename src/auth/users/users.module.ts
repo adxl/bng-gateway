536089121a480cc35ba17ccdb28d3a712b60997f
Coraline <cesedji@myges.fr>
2023-06-27 09:50:45 +0200

    Fix : local config

--- src/auth/users/users.module.ts
+++ src/auth/users/users.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ClientProxy } from 'src/config/proxy.config';
 import { AUTH_SERVICE } from 'src/constants';
 import { UsersController } from './users.controller';
-import { ClientProxy } from 'src/config/proxy.config';
 
 @Module({
-  imports: [ClientProxy(AUTH_SERVICE, process.env.AUTH_SERVICE || 'auth-api-service', process.env.AUTH_PORT)],
+  imports: [ClientProxy(AUTH_SERVICE, process.env.AUTH_HOST || 'auth-api-service', process.env.AUTH_PORT)],
   controllers: [UsersController],
 })
 export class UsersModule {}
