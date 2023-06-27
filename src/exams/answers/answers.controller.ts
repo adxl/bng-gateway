2d379894a19b34219ee8a3104e1c200472b72a7b
Coraline <cesedji@myges.fr>
2023-06-28 01:32:18 +0200

    Fix  : exams routes

--- src/exams/answers/answers.controller.ts
+++ src/exams/answers/answers.controller.ts
@@ -10,21 +10,21 @@ export class AnswersController {
 
   @Get(':id')
   public findOne(@Param('id', ParseUUIDPipe) id: string, @Headers('authorization') token: string) {
-    return this.answersProxy.send('exams.findOne', { id, token }).pipe(catchRpcException);
+    return this.answersProxy.send('answers.findOne', { id, token }).pipe(catchRpcException);
   }
 
   @Post()
   public create(@Headers('authorization') token: string, @Body() body: AbstractBody) {
-    return this.answersProxy.send('exams.create', { token, body }).pipe(catchRpcException);
+    return this.answersProxy.send('answers.create', { token, body }).pipe(catchRpcException);
   }
 
   @Patch(':id')
   public update(@Param('id', ParseUUIDPipe) id: string, @Headers('authorization') token: string, @Body() body: AbstractBody) {
-    return this.answersProxy.send('exams.update', { id, token, body }).pipe(catchRpcException);
+    return this.answersProxy.send('answers.update', { id, token, body }).pipe(catchRpcException);
   }
 
-  @Delete()
+  @Delete(':id')
   public remove(@Param('id', ParseUUIDPipe) id: string, @Headers('authorization') token: string) {
-    return this.answersProxy.send('exams.remove', { id, token }).pipe(catchRpcException);
+    return this.answersProxy.send('answers.remove', { id, token }).pipe(catchRpcException);
   }
 }
