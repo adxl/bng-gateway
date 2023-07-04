950d2b0f72d42422a069055edd5135e2757df79d
Coraline <cesedji@myges.fr>
2023-07-04 21:22:23 +0200

    Feat : update auction controllers

--- src/gears/auctions/auctions.controller.ts
+++ src/gears/auctions/auctions.controller.ts
@@ -4,7 +4,7 @@ import { GEARS_SERVICE } from 'src/constants';
 import { catchRpcException } from 'src/exceptions/exceptions.pipe';
 import { AbstractBody } from 'src/types';
 
-@Controller('gears/auctions')
+@Controller('gears/auctions/')
 export class AuctionsController {
   public constructor(@Inject(GEARS_SERVICE) private readonly gearsProxy: ClientProxy) {}
 
@@ -13,7 +13,12 @@ export class AuctionsController {
     return this.gearsProxy.send('auctions.findAll', { token }).pipe(catchRpcException);
   }
 
-  @Get('/:id')
+  @Get('active')
+  public findActive(@Headers('authorization') token: string) {
+    return this.gearsProxy.send('auctions.findActive', { token }).pipe(catchRpcException);
+  }
+
+  @Get(':id')
   public findOne(@Param('id', ParseUUIDPipe) id: string, @Headers('authorization') token: string) {
     return this.gearsProxy.send('auctions.findOne', { id, token }).pipe(catchRpcException);
   }
@@ -23,7 +28,7 @@ export class AuctionsController {
     return this.gearsProxy.send('auctions.create', { token, body }).pipe(catchRpcException);
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   public close(@Param('id', ParseUUIDPipe) id: string, @Headers('authorization') token: string) {
     return this.gearsProxy.send('auctions.close', { id, token }).pipe(catchRpcException);
   }
